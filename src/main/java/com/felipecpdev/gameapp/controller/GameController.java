f61f854c217a142b6131c6ec0de6974a3797fecf
Felipe Contreras <felipe-contreras.dev@gmail.com>
2023-07-26 17:48:48 -0400

    CrossOrigin

--- src/main/java/com/felipecpdev/gameapp/controller/GameController.java
+++ src/main/java/com/felipecpdev/gameapp/controller/GameController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/games")
+@CrossOrigin(origins = "http://localhost:4200")
 public class GameController {
 
     private final GameService gameService;
